a2a4e05246797bcb3cad7cf06e4652f497554b4e
Fil Veith <fil.veith@etu.u-bordeaux.fr>
2021-03-25 14:35:06 +0100

    ajout des chiffres et lettres avec ecran carree

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        Screen s = new Screen(25);
+        Screen s = new Screen(5);  //max 26 because the alphabet is 26 char long :)
         Match m = new Match();
         s.clear();
         s.display();
--- src/Screen.java
+++ src/Screen.java
@@ -12,12 +12,12 @@ public class Screen {
 
     Screen(int size_) {
         size = size_;
-        this.image = new char[size][size];
+        this.image = new char[size*2][size*2];
     }
 
     public void clear() {
         for(int r = 0; r < size; r++) {
-            for(int c = 0; c < size ; c++) {
+            for(int c = 0; c < size*2; c++) {
                 image[c][r] = ' ';
                 }
             }
@@ -33,31 +33,27 @@ public class Screen {
                  lineNum = (" "+Integer.toString(r+1));
             }
             System.out.print(lineNum+"|");
-            for(int c = 0; c < size ; c++) {
+            for(int c = 0; c < size*2 ; c++) {
                 System.out.print(image[c][r]);
                 }
-            System.out.println("|");
+            System.out.println(" |");
             }
             displayBar();
         }
     
     private void displayBar() {
         System.out.print("  +");
-        for(int c = 0; c < size ; c++) {
+        for(int c = 0; c < size*2 ; c++) {
             System.out.print("-");
         }
-        System.out.println("+");
+        System.out.println("-+");
     }
 
     private void displayLetters(){
         System.out.print("   ");
-        for(int i = 0; i<size/2; i++){
+        for(int i = 0; i<size; i++){
             System.out.print(" "+alphabet[i]);
         }
         System.out.println("");
-    }
-
-    private void displayNumbers(){
-
     }
 }
