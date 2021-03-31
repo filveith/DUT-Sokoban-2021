f82414e8be00560a3b3af895f4f65e0e1989c109
Fil Veith <fveith@etu.u-bordeaux.fr>
2021-03-31 13:44:40 +0200

    ajout class Joueur To Do: ajout choix joueur

--- src/jeu/Main.java
+++ src/jeu/Main.java
@@ -9,13 +9,19 @@ public class Main {
     public static void main(String[] args) {
         UI m = new UI();    
         Screen s = new Screen(bootMain());  //max 26 because the alphabet is 26 char long :)
+        Joueur j1 = new Joueur("Joueur1",0,'X');
+        Joueur j2 = new Joueur("Joueur2",1,'O');
         s.clear();
         s.display();
+
+        int playersTurn = 1;
+
         
         System.out.println("* Menu *");
         while(true){
             Game.getAllPlayableMoves(s);
             String userInput = m.userInterface();      
+            
             if (userInput.length() == 2){
                 x = userInput.charAt(0);
                 y = Integer.parseInt(userInput.substring(1));
@@ -23,9 +29,18 @@ public class Main {
                 x = userInput.charAt(0);
                 y = Integer.parseInt(userInput.substring(1,3));
             }
+
             if(s.checkUserInput(x, y)){
                 s.setPoint(x, y);
                 s.display();
+                if (playersTurn == 1) {
+                    j1.setCoupJouee(userInput);
+                    playersTurn++;
+                } else if (playersTurn == 2) {
+                    j2.setCoupJouee(userInput);
+                    playersTurn--;
+                }
+
             } else {
                 System.out.println("Erreur: Le point sélectionné n'est pas disponible");
             }
