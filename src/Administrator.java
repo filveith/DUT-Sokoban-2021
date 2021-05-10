c0a3015b162a1a829a1861e4c6e063a71ab74dcb
Fil Veith <fveith@etu.u-bordeaux.fr>
2021-05-10 17:58:02 +0200

    sqlite working added close connection in main

--- src/Administrator.java
+++ src/Administrator.java
@@ -19,6 +19,7 @@ public class Administrator {
       showDataBase();
       updateDataBase();
       // showDataBase();
+      closeDataBase();
    }
 
    private static void initDataBaseConnection() {
